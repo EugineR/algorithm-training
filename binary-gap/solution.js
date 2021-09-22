e48dbb5bbce2fc3d33052c3d8e6c39b668f3b4c6
Eugene Reshentyak <euginer96@gmail.com>
2021-09-22 21:10:46 +0200

    Binary gap

---
+++ binary-gap/solution.js
@@ -0,0 +1,27 @@
+function solution(N) {
+
+    let maxGap = 0;
+    let lastTheOne;
+    let index = 0;
+
+    while (N >= 1) {
+        const mod = N % 2;
+
+        if (mod === 1) {
+            if (lastTheOne !== undefined) {
+                const newGap = index - lastTheOne - 1;
+
+                if (newGap > maxGap) {
+                    maxGap = newGap;
+                }
+            }
+
+            lastTheOne = index;
+        }
+
+        N = Math.trunc(N / 2);
+        index++;
+    }
+
+    return maxGap;
+}
