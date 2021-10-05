8faf4e2370443bbae30c37e24e93bdb52cdc8812
Eugene Reshentyak <euginer96@gmail.com>
2021-10-05 17:09:39 +0200

    FrogRiverOne 54%

--- FrogRiverOne/solution.js
+++ FrogRiverOne/solution.js
@@ -1,6 +1,8 @@
 // https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 
-function solution(X, A) {
+
+// 72%
+function solution72(X, A) {
     let result = -1;
 
     const leavesCountingArray = new Array(X).fill(0);
@@ -11,6 +13,38 @@ function solution(X, A) {
         }
 
         const isAllLeavesExist = leavesCountingArray.every(item => item > 0)
+
+        if (isAllLeavesExist) {
+            result = index;
+            return true
+        } else {
+            return false
+        }
+
+    })
+
+    return result;
+}
+
+// 54%
+function solution54(X, A) {
+    let result = -1;
+
+    const resultStorage = new Array(X)
+        .fill(0)
+        .map((item, index) => index + 1)
+        .reduce((acc, item) => ({
+            ...acc,
+            [item]: item
+        }), {})
+
+
+    A.some((item, index) => {
+        if (!!resultStorage[item]) {
+            delete resultStorage[item];
+        }
+
+        const isAllLeavesExist = !Object.keys(resultStorage).length;
 
         if (isAllLeavesExist) {
             result = index;
