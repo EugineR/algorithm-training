3cdc7bd12801aaf0c569c21e39e38f2c4d061aea
Eugene Reshentyak <euginer96@gmail.com>
2021-10-11 19:47:36 +0200

    PermCheck 100%

--- MaxCounters/solution.js
+++ MaxCounters/solution.js
@@ -1,3 +1,4 @@
+// https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
 // 77%
 function solution(N, A) {
     const array = new Array(N).fill(0);
@@ -17,6 +18,35 @@ function solution(N, A) {
             lastMax = array[index]
         }
     })
+
+    return array.map(item => item < currentMax ? currentMax : item);
+}
+
+// 100%
+
+function solution(N, A) {
+    const array = new Array(N).fill(0);
+    let lastMax = 0;
+    let currentMax = 0;
+
+    A.forEach(item => {
+        const index = item - 1;
+
+        if (item > N) {
+            currentMax = lastMax
+        } else {
+            if (array[index] <= currentMax) {
+                array[index] = currentMax + 1
+            } else {
+                array[index]++
+            }
+        }
+
+        if (array[index] > lastMax) {
+            lastMax = array[index]
+        }
+    })
+
 
     return array.map(item => item < currentMax ? currentMax : item);
 }
