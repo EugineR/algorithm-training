f1a4f0186fa22e1458076fac4f4eb8f95afca206
Eugene Reshentyak <euginer96@gmail.com>
2021-10-20 19:43:48 +0200

    MaxProductOfThree 100%

--- MaxProductOfThree/solution.js
+++ MaxProductOfThree/solution.js
@@ -7,3 +7,18 @@ function solution(A) {
 
     return positiveMax > negativeMax ? positiveMax : negativeMax;
 }
+
+// 100%
+
+function solution(A) {
+    const comparator = (a, b) =>  a - b;
+    const sortedA = A.sort(comparator);
+
+    const negativeMax = sortedA[0] * sortedA[1] * sortedA[A.length - 1];
+    const allNegativeMax = sortedA[0] * sortedA[1] * sortedA[2];
+    const positiveMax = sortedA[A.length - 1] * sortedA[A.length - 2] * sortedA[A.length - 3];
+
+    const results = [negativeMax, positiveMax, allNegativeMax].sort(comparator)
+
+    return results[results.length - 1];
+}
