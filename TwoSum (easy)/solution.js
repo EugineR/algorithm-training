ef2f4a730d0fdd6a3200b3954b47be13d81c336f
Eugene Reshentyak <euginer96@gmail.com>
2024-11-02 23:25:58 +0100

    Two Sum - 75%

--- TwoSum (easy)/solution.js
+++ TwoSum (easy)/solution.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/two-sum/
 
+// 5%
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -12,6 +13,27 @@ var twoSum = function (nums, target) {
         rightIndex = nums.findLastIndex(item => item === x);
         if(rightIndex > 0 && rightIndex !== i) {
             return [i, rightIndex];
+        }
+    }
+};
+
+// 75%
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const map = {};
+    let x, rightIndex;
+
+    for (let i = 0; i < nums.length; i++) {
+        x = target - nums[i];
+
+        if(map[x] === undefined) {
+            map[nums[i]] = i;
+        } else {
+            return [map[x], i]
         }
     }
 };
