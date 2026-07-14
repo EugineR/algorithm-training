12369a917be6a0c5179c0f8e71a920f0e8fa5330
Evgenii Reshetniak <euginer96@gmail.com>
2026-07-14 14:31:18 +0200

    Integer to Roman - 84%

--- Integer to Roman/Solution.js
+++ Integer to Roman/Solution.js
@@ -41,3 +41,32 @@ var intToRoman = function(num) {
 
     return str;
 };
+
+// 84%
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let temp = num;
+    let str = '';
+
+    const numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const chars = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    while (temp !== 0) {
+        for(let i = 0; i < numbers.length; i++) {
+            result = Math.floor(temp / numbers[i]);
+
+            if (result > 0 && result < 4) {
+                for (let j = 0; j < result; j++) {
+                    str = str + chars[i];
+                }
+
+                temp = temp - result * numbers[i];
+            }
+        }
+    }
+
+    return str;
+};
