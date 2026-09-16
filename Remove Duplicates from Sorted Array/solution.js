882d8ff559ec524738746877eac95a02c69e012c
Evgenii Reshetniak <evgeniireshetniak@cloud.upwork.com>
2026-09-16 22:09:36 +0200

    Remove Duplicates from Sorted Array - 48%, memory 42%

---
+++ Remove Duplicates from Sorted Array/solution.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 48% / 42%
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if(nums.length === 1) {
+    return 1;
+  }
+
+  let k = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if(nums[i] !== nums[k]) {
+      k++;
+      nums[k] = nums[i];
+    }
+  }
+
+  return k+1;
+
+};
