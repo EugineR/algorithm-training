9f9d176d07244efccc71c9d6e6e3d1377c05a6be
Evgenii Reshetniak <evgeniireshetniak@cloud.upwork.com>
2026-09-16 22:11:43 +0200

    Remove Duplicates from Sorted Array - 48%, memory 73%

--- Remove Duplicates from Sorted Array/solution.js
+++ Remove Duplicates from Sorted Array/solution.js
@@ -6,20 +6,38 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-  if(nums.length === 1) {
+var removeDuplicates = function (nums) {
+  if (nums.length === 1) {
     return 1;
   }
 
   let k = 0;
 
   for (let i = 1; i < nums.length; i++) {
-    if(nums[i] !== nums[k]) {
+    if (nums[i] !== nums[k]) {
+      k++;
+      nums[k] = nums[i];
+    }
+  }
+  return k + 1;
+};
+
+
+// 48% / 73%
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[k]) {
       k++;
       nums[k] = nums[i];
     }
   }
 
-  return k+1;
-
+  return k + 1;
 };
