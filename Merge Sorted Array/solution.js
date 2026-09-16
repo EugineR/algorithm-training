b139b944f81ca8cf052a83c36c798442b5564ba5
Evgenii Reshetniak <evgeniireshetniak@cloud.upwork.com>
2026-09-16 20:21:03 +0200

    Merge Sorted Array - 100%, but memory 89%

--- Merge Sorted Array/solution.js
+++ Merge Sorted Array/solution.js
@@ -75,6 +75,41 @@ var merge = function(nums1, m, nums2, n) {
   }
 };
 
+// 100% / 89%
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  m--;
+  n--;
+
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if(m < 0) {
+      nums1[i] = nums2[n];
+      n--;
+      continue
+    }
+
+    if(n < 0) {
+      nums1[i] = nums1[m];
+      m--;
+      continue
+    }
+
+    if(nums1[m] > nums2[n]) {
+      nums1[i] = nums1[m];
+      m--;
+    } else {
+      nums1[i] = nums2[n];
+      n--;
+    }
+  }
+};
 
 
 
