c19aad288094e51416386c7f0c9c714a59388300
Evgenii Reshetniak <evgeniireshetniak@cloud.upwork.com>
2026-09-16 20:55:57 +0200

    Remove Element - 100%, but memory 53%

---
+++ Remove Element/solution.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+// 100% / 53%
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let k = 0;
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if(j >= nums.length) {
+
+      return k;
+    }
+
+    while (nums[j] === val && j < nums.length) {
+      j++;
+    }
+
+    if(nums[j] !== val && j < nums.length) {
+      nums[i] = nums[j];
+      j++;
+      k++;
+    }
+  }
+
+  return k;
+}
+
+
